be9ddaa3b7afdba460f595b6ba74f6437cf3373e
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-20 19:06:29 -0800

    commit tree 769e1b0fab309acb7b30b5d862073ced45c6077d

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+class Song 
+
+    attr_reader :name
+    attr_accessor :artist, :song
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @artist = artist
+        @@all << self  
+    end 
+
+    def self.all 
+        @@all
+    end 
+
+    def artist_name 
+        if self.artist 
+            self.artist.name 
+        else 
+            nil 
+        end 
+    end
+
+end 
