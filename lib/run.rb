b4dc12a60dd76858e9cde51d51e562a251490789
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-20 19:08:35 -0800

    all tests pass, objects have many relationships

---
+++ lib/run.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require_relative "../lib/artist.rb"
+require_relative "../lib/song.rb"
+require_relative "../lib/author.rb"
+require_relative "../lib/post.rb"
+
+hello = Song.new("Hello") 
+hello.artist = "Adele" 
